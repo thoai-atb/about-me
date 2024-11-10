c060afcd6dcea9d44992a60551881c3d0d89fbc7
Lý Bảo Thoại <lybaothoai@gmail.com>
2024-11-10 15:34:24 +0700

    Get basePath from github env

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
 import type { NextConfig } from "next";
 
+const githubRepository = process.env.GITHUB_REPOSITORY || "thoai/haha";
+
+// Extract only the repository name after the slash
+const repoName = "/" + (githubRepository.split("/").pop() || "");
+
+console.log("REPO NAME: " + repoName);
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/about-me'
+  basePath: repoName || '',
 };
 
 export default nextConfig;

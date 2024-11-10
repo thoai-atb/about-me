dfaa4f86d615f7ed3bf0839f3242464ea38d8de1
Lý Bảo Thoại <lybaothoai@gmail.com>
2024-11-10 20:52:54 +0700

    Use publicRuntimeConfig for basePath

--- app/components/BasePathImage.tsx
+++ app/components/BasePathImage.tsx
@@ -1,16 +1,24 @@
-import Image, { ImageProps } from 'next/image';
-import getConfig from 'next/config';
+import Image, { ImageProps } from "next/image";
+import getConfig from "next/config";
 
+const { publicRuntimeConfig } = getConfig();
+const { basePath } = publicRuntimeConfig;
 
 interface BasePathImageProps extends ImageProps {
   src: string; // Override `src` to ensure it's required
 }
 
-const { basePath } = getConfig();
-
-const BasePathImage: React.FC<BasePathImageProps> = ({ src, alt, width, height, priority = false, className = '' }) => {
+const BasePathImage: React.FC<BasePathImageProps> = ({
+  src,
+  alt,
+  width,
+  height,
+  priority = false,
+  className = "",
+}) => {
   const appending = basePath ? basePath : "";
   const imageSrc = `${appending}${src}`;
+  console.log(imageSrc);
 
   return (
     <Image

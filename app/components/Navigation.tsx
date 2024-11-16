884721c87554eaa4adcc6e5bb9cf16a660203bd7
Lý Bảo Thoại <lybaothoai@gmail.com>
2024-11-16 16:31:09 +0700

    Change to light mode, change title

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,21 +1,21 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Navigation() {
   const navItems = [
-    { name: "Home", href: "/" },
-    { name: "Projects", href: "/projects" },
-    { name: "Experiences", href: "/experiences" },
-    { name: "Hobbies", href: "/hobbies" },
+    { name: 'Home', href: '/' },
+    { name: 'Projects', href: '/projects' },
+    { name: 'Experiences', href: '/experiences' },
+    { name: 'Hobbies', href: '/hobbies' },
   ];
 
   return (
-    <nav className="bg-gray-800 shadow-md">
+    <nav className="bg-gray-200 shadow-md">
       <div className="flex justify-around items-center py-4">
         {navItems.map((item) => (
           <Link
             key={item.name}
             href={item.href}
-            className="text-lg font-semibold hover:text-gray-400 cursor-pointer"
+            className="text-lg font-semibold text-gray-700 hover:text-indigo-600 cursor-pointer"
           >
             {item.name}
           </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@ import BasePathImage from './components/BasePathImage';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-black text-white font-sans">
+    <div className="min-h-screen bg-white text-gray-900 font-sans">
       {/* Navigation Bar */}
       <Navigation />
+      
       {/* Hero Section */}
-      <div className="flex flex-col md:flex-row items-center p-8 bg-gray-900">
+      <div className="flex flex-col md:flex-row items-center p-8 bg-gray-100">
         <BasePathImage
-          className="rounded-full border-4 border-gray-700 shadow-lg"
+          className="rounded-full border-4 border-gray-300 shadow-lg"
           src={`/thoai.png`}
           alt="Thoai"
           width={180}
@@ -17,7 +18,7 @@ export default function Home() {
           priority
         />
         <div className="text-center md:text-left text-3xl md:text-5xl font-mono mt-6 md:mt-0 md:ml-8 leading-snug">
-          Hi, I am <span className="text-indigo-400">Thoai</span>. <br />
+          Hi, I am <span className="text-indigo-600">Thoai</span>. <br />
           Welcome to my website!
         </div>
       </div>

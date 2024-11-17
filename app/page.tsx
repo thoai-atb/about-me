d3fa3ef2f728cebf03ea4f0ef527acc2991fbbc3
Lý Bảo Thoại <lybaothoai@gmail.com>
2024-11-17 22:05:54 +0700

    Change basePath to using env, add more project pictures, add image modal

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
-import Navigation from './components/Navigation';
-import BasePathImage from './components/BasePathImage';
+import Navigation from "./components/Navigation";
+import BasePathImage from "./components/BasePathImage";
 
 export default function Home() {
   return (
     <div className="min-h-screen text-gray-900 font-sans">
       {/* Navigation Bar */}
       <Navigation />
-      
+
       {/* Hero Section */}
       <div className="flex flex-col md:flex-row items-center p-8 bg-gray-100">
         <BasePathImage
-          className="rounded-full border-4 border-gray-300 shadow-lg"
+          className="rounded-full w-40 h-40 border-4 border-gray-300 shadow-lg"
           src={`/thoai.png`}
           alt="Thoai"
-          width={180}
-          height={180}
+          // width={180}
+          // height={10}
           priority
         />
         <div className="text-center md:text-left text-3xl md:text-5xl font-mono mt-6 md:mt-0 md:ml-8 leading-snug">
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,51 +1,78 @@
-import React from 'react';
-import Navigation from '../components/Navigation';
-import ProjectCard from '../components/ProjectCard';
-import PageContainer from '../components/PageContainer';
+"use client";
+import Navigation from "../components/Navigation";
+import ProjectCard from "../components/ProjectCard";
+import PageContainer from "../components/PageContainer";
+import ImageModal from "../components/ImageModal";
+import { useState } from "react";
+
+const projects = [
+  {
+    title: "Maze Escaper",
+    description:
+      "This is a brief description of the project, highlighting its purpose and key features.",
+    imageSrc: "/maze_escaper.png",
+    imageAlt: "Representative image of the project",
+  },
+  {
+    title: "Texas Holdem",
+    description:
+      "This is a brief description of the project, highlighting its purpose and key features.",
+    imageSrc: "/texas_holdem.png",
+    imageAlt: "Representative image of the project",
+  },
+  {
+    title: "Student Learning Progress System",
+    description:
+      "This is a brief description of the project, highlighting its purpose and key features.",
+    imageSrc: "/student_learning_progress.png",
+    imageAlt: "Representative image of the project",
+  },
+  {
+    title: "Rubik's Cube OLL Distribution",
+    description:
+      "This is a brief description of the project, highlighting its purpose and key features.",
+    imageSrc: "/oll_distribution.png",
+    imageAlt: "Representative image of the project",
+  },
+];
 
 export default function Projects() {
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
+
+  console.log(imageSrc);
+
+  const handleModalClose = () => {
+    setImageSrc(null); // Close the modal by setting the image source to null
+  };
+
   return (
     <div className="min-h-screen bg-white text-gray-900 font-sans">
-      {/* Navigation Bar */}
       <Navigation />
-
-      {/* Projects Section */}
       <PageContainer>
-
-        <h1 className="text-3xl font-bold text-center mb-8">My Projects</h1>
-
-        {/* Example Project */}
-        <ProjectCard
-          title="Maze Escaper"
-          description="This is a brief description of the project, highlighting its purpose and key features."
-          imageSrc="/maze_escaper.png" // Replace with your image path
-          imageAlt="Representative image of the project"
-          flip={true}
-        />
-        <ProjectCard
-          title="Awesome Project"
-          description="This is a brief description of the project, highlighting its purpose and key features."
-          imageSrc="/maze_escaper.png" // Replace with your image path
-          imageAlt="Representative image of the project"
-          flip={false}
-        />
-        <ProjectCard
-          title="Awesome Project"
-          description="This is a brief description of the project, highlighting its purpose and key features."
-          imageSrc="/maze_escaper.png" // Replace with your image path
-          imageAlt="Representative image of the project"
-          flip={true}
-        />
-        <ProjectCard
-          title="Awesome Project"
-          description="This is a brief description of the project, highlighting its purpose and key features."
-          imageSrc="/maze_escaper.png" // Replace with your image path
-          imageAlt="Representative image of the project"
-          flip={false}
-        />
-
-        {/* Add more <ProjectCard /> components for additional projects */}
+        <h1 className="text-3xl font-bold text-center mb-8">
+          My Projects
+        </h1>
+        <div>
+          {projects.map((project, index) => (
+            <ProjectCard
+              key={project.title}
+              title={project.title}
+              description={project.description}
+              imageSrc={project.imageSrc}
+              imageAlt={project.imageAlt}
+              flip={index % 2 === 0}
+              onImageClick={() => setImageSrc(project.imageSrc)}
+            />
+          ))}
+        </div>
       </PageContainer>
+
+      {imageSrc && (
+        <ImageModal
+          imageSrc={imageSrc}
+          onClose={() => handleModalClose()} // Pass close handler to modal
+        />
+      )}
     </div>
   );
 }
